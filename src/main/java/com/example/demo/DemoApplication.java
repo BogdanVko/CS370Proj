446c23b38d6ebee3a2049027c3d521aacaf355bd
BogdanPC <vba@colostate.edu>
2020-10-19 19:41:35 -0600

    Not complete TutorList added
    Does not compile yet :)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
 
 @SpringBootApplication
+@ComponentScan("com.example.demo.repository")//to scan repository files
+@EntityScan("com.example.entity")
+@EnableJpaRepositories("com.example.demo")
 public class DemoApplication {
+
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -15,57 +15,17 @@ import java.util.Date;
 ///Tasks
 @Controller
 public class MainController {
-<<<<<<< Updated upstream
+
     private int count = 0;
     private int count1 = 0;
-=======
-    private int count = 10;
->>>>>>> Stashed changes
 
-    @GetMapping("/main")
-    public String mainController(ModelMap mp){
-        mp.addAttribute("test", count);
-        return "main"; // Error 500
+
+
+
+    @GetMapping("/")
+    public String getIndex(){
+        return "index";
     }
-
-
-
-    @GetMapping("/alek") // Get request
-    public String setTest(ModelMap mp) {
-        count+=10;
-        mp.addAttribute("test", count);
-
-
-        return "main";
-    }
-    @GetMapping("/wordchange") // Get request
-    public String setWord(ModelMap mp) {
-        String Alec;
-        count1++;
-        if(count1%2 == 0){
-            Alec = "Back";
-            mp.addAttribute("Word", Alec);
-        }
-        else{
-            Alec = "Forth";
-            mp.addAttribute("Word", Alec);
-        }
-        return "Alec";
-    }
-
-    @GetMapping("/Alec")
-    public String Alec(){
-        return "Alec"; // Error 500
-    }
-
-
-
-    @GetMapping("/hello")
-    public String helloWorld(){
-
-        return "main"; // Error 500
-    }
-
 
 
 }
